e1f6e49c183bede1d2aaea2f9c4ee1b86f6a22df
Micabuzer <58298161+Micabuzer@users.noreply.github.com>
2020-11-06 23:42:00 +0800

    master

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\danil\onedrive\documents\first_django_project\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
